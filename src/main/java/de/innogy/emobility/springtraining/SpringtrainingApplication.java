55feb216db43404b14ea6b34464317546a5a8e8b
Niels Buchholz <niels.buchholz.extern@innogy.com>
2018-06-28 10:07:30 +0200

    init

---
+++ src/main/java/de/innogy/emobility/springtraining/SpringtrainingApplication.java
@@ -0,0 +1,12 @@
+package de.innogy.emobility.springtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtrainingApplication.class, args);
+	}
+}
